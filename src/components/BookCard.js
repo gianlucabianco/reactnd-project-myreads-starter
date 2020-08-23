0be49924d5a7a1b279a18e13ec05f9982643378f
Gianluca <gianluca@MBPdiFrancesco2.station>
2020-08-23 15:17:02 +0200

    feat: BookShelfChanger component imported in BookCard

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import BookShelfChanger from './BookShelfChanger';
+
 class BookCard extends React.Component {
 
     render() {
@@ -25,16 +27,9 @@ class BookCard extends React.Component {
                         backgroundImage: `url("${ cover }")`,
                     }}
                 />
-                {/* TODO: this should be a compoent ? */}
-                <div className="book-shelf-changer">
-                    <select>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                    </select>
-                </div>
+
+                <BookShelfChanger />
+                
                 </div>
 
                 <div className="book-title" >
