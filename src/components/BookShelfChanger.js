ee6a17ccb74bd7ebb72385e7532305304415c2a2
Gianluca <gianluca@MBPdiFrancesco2.station>
2020-08-24 15:12:39 +0200

    feat: new Props && handleChange() added.

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -2,16 +2,32 @@ import React from 'react';
 
 class BookShelfChanger extends React.Component {
 
+    handleChange = event => {
+
+        const newShelf = event.target.value;
+
+        if ( newShelf !== 'None' ) {
+            
+            this.props.onShelfChange( newShelf );
+
+        }
+
+
+      };
+
     render() {
 
         return (
             <div className="book-shelf-changer">
-                <select>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
+                <select
+                    onChange={ this.handleChange }
+                    defaultValue={ this.props.shelfName }                    
+                >
+                    <option value="Move" disabled>Move to...</option>
+                    <option value="Currently Reading">Currently Reading</option>
+                    <option value="Want to Read">Want to Read</option>
+                    <option value="Read">Read</option>
+                    <option value="None">None</option>
                 </select>
             </div>
         );
