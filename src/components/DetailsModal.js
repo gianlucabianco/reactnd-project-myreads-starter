71433b1bb852d2e3b0f9c5f9fe7fde471b8997c3
Gianluca <gianluca@MBPdiFrancesco2.station>
2020-08-26 17:31:45 +0200

    feat: <DetailsModal> template

--- src/components/DetailsModal.js
+++ src/components/DetailsModal.js
@@ -16,11 +16,101 @@ class DetailsModal extends React.Component {
 
         return (
             <div className="details-modal">
-                this is a modal for { book.title }
-                <button
-                    onClick={ event => this.hideDetails( event ) }
+                <div className="details-modal-content">
+                    <h2>Book details</h2>
+                    
+                    {
+                        book.title
+                        && <div>
+                            <strong>Title: </strong> { book.title }
+                        </div>
+                    }
 
-                >close</button>
+                    {
+                        book.subtitle
+                        && <div>
+                            <strong>Subtitle: </strong> { book.subtitle }
+                        </div>
+                    }
+
+                    {
+                        book.pageCount
+                        && <div>
+                            <strong>Page count: </strong> { book.pageCount }
+                        </div>
+                    }
+
+                    {
+                        book.authors
+                        && book.authors.length
+                        && <div className="details-modal-content-authors">
+                                <strong>Authors:</strong> {
+                                    book.authors.map(
+                                        author => (
+                                            <div
+                                                key={ author }
+                                            >
+                                                { author }
+                                            </div>
+                                        )
+                                    )
+                                }
+                        </div>
+                    }
+
+                    {
+                        book.categories
+                        && book.categories.length
+                        && <div className="details-modal-content-categories">
+                            <strong>Categories:</strong> {
+                                book.categories.map(
+                                    category => (
+                                        <div
+                                            key={ category }
+                                        >
+                                            { category }
+                                        </div>
+                                    )
+                                )
+                            }
+                        </div>
+                    }
+
+                    {
+                        book.averageRating
+                        && <div>
+                            <strong>Average Rating: </strong> { book.averageRating }/5
+                        </div>
+                    }
+
+                    {
+                        book.description
+                        && <div className="details-modal-content-description">
+                            <strong>Description: </strong>
+                            <span>{ book.description }</span>
+                        </div>
+                    }
+
+                    {
+                        book.publishedDate
+                        && <div>
+                            <strong>Published Date: </strong> { book.publishedDate }
+                        </div>
+                    }
+
+                    {
+                        book.publisher
+                        && <div>
+                            <strong>Publisher: </strong> { book.publisher }
+                        </div>
+                    }
+
+                    <button
+                        className="close-modal-button"
+                        onClick={ event => this.hideDetails( event ) }
+
+                    >Back to books</button>
+                </div>
             </div>
         );
 
