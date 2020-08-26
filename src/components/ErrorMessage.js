3cd915c6ca0d41dace2ae9f1ffb68ae4c064f7f3
Gianluca <gianluca@MBPdiFrancesco2.station>
2020-08-26 10:54:31 +0200

    FEAT: error message component

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,126 @@
+import React from 'react';
+
+class ErrorMessage extends React.Component {
+
+    
+    render() {
+
+        /* Note for the Reviewer: "hints" should come from a props / BE / API */        
+        const hints = [
+            'Android',
+            'Art',
+            'Artificial Intelligence',
+            'Astronomy',
+            'Austen',
+            'Baseball',
+            'Basketball',
+            'Bhagat',
+            'Biography',
+            'Brief',
+            'Business',
+            'Camus',
+            'Cervantes',
+            'Christie',
+            'Classics',
+            'Comics',
+            'Cook',
+            'Cricket',
+            'Cycling',
+            'Desai',
+            'Design',
+            'Development',
+            'Digital Marketing',
+            'Drama',
+            'Drawing',
+            'Dumas',
+            'Education',
+            'Everything',
+            'Fantasy',
+            'Film',
+            'Finance',
+            'First',
+            'Fitness',
+            'Football',
+            'Future',
+            'Games',
+            'Gandhi',
+            'Homer',
+            'Horror',
+            'Hugo',
+            'Ibsen',
+            'Journey',
+            'Kafka',
+            'King',
+            'Lahiri',
+            'Larsson',
+            'Learn',
+            'Literary Fiction',
+            'Make',
+            'Manage',
+            'Marquez',
+            'Money',
+            'Mystery',
+            'Negotiate',
+            'Painting',
+            'Philosophy',
+            'Photography',
+            'Poetry',
+            'Production',
+            'Programming',
+            'React',
+            'Redux',
+            'River',
+            'Robotics',
+            'Rowling',
+            'Satire',
+            'Science Fiction',
+            'Shakespeare',
+            'Singh',
+            'Swimming',
+            'Tale',
+            'Thrun',
+            'Time',
+            'Tolstoy',
+            'Travel',
+            'Ultimate',
+            'Virtual Reality',
+            'Web Development',
+            'iOS'
+        ]
+
+        return (
+            <div
+                className="error-message"
+            >
+                <p
+                    className="error-message-title"
+                >
+                    { this.props.message }
+                </p>
+                <div
+                    className="error-message-hints"
+                >
+                    <div>
+                        Try something like this:
+                    </div>                
+                    {
+                        this.props.isError
+                        && hints.map(
+                            (
+                                hint,
+                                index
+                            ) => (
+                                <span
+                                    key={ `${ hint }-${ index }` }
+                                >{`${ hint } | `}</span>
+                            )
+                        )
+                    }
+                </div>
+            </div>
+        );
+    }
+
+};
+
+export default ErrorMessage;
